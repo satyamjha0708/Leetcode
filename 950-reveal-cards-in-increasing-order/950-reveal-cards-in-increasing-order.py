2bf626e6a76f50dbc84796658c74d9558b3449d7
Satyam Jha <61003236+satyamjha0708@users.noreply.github.com>
2022-06-30 16:33:05 +0530

    Time: 52 ms (80.93%), Space: 14 MB (88.01%) - LeetHub

---
+++ 950-reveal-cards-in-increasing-order/950-reveal-cards-in-increasing-order.py
@@ -0,0 +1,13 @@
+import collections
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        d=collections.deque()
+        
+        for x in sorted(deck)[::-1]:
+            d.rotate()
+            d.appendleft(x)
+            
+            
+        return list(d)
+        
+        
