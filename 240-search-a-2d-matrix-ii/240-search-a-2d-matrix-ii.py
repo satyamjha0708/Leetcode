4e6809511c0879680c70033abb62dde1cbe0d1e1
Satyam Jha <61003236+satyamjha0708@users.noreply.github.com>
2022-08-03 16:22:17 +0530

    Time: 350 ms (15.22%), Space: 20.3 MB (83.03%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,11 +1,36 @@
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j]==target:
-                    return True
-                
-        else:
+
+    def searchMatrix(self, matrix: List[List[int]], target: int)-> bool:
+
+        
+        if not len(matrix) or not len(matrix[0]):
+			# Quick response for empty matrix
             return False
         
+        h, w = len(matrix), len(matrix[0])
+        
+        # Start adaptive search from bottom left corner
+        y, x = h-1, 0
+        
+        while True:
+            
+            if y < 0 or x >= w:
+                break
+            
+            current = matrix[y][x]
+            
+            if target < current:
+                # target is smaller, then go up
+                y -= 1
+            
+            elif target > current:
+                # target is larger, then go right
+                x += 1
+            
+            else:
+                # hit target
+                return True
+            
+        return False
+    
         
