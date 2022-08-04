a6edf74562eeabae88245937702ded93606538da
Satyam Jha <61003236+satyamjha0708@users.noreply.github.com>
2022-08-04 11:34:51 +0530

    Time: 185 ms (90.22%), Space: 23.7 MB (90.37%) - LeetHub

---
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        xor=0
+        
+        for i in nums:
+            xor^=i
+            
+            
+            
+        return xor
+        
