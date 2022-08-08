14110e916ead5e8559799dec22842c0e2e0d5174
Satyam Jha <61003236+satyamjha0708@users.noreply.github.com>
2022-08-08 20:07:25 +0530

    Time: 77 ms (23.82%), Space: 14 MB (15.05%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        def func1(arr,num):
+            for i in range(len(arr)):
+                if arr[i]>0:
+                    arr[i]=arr[i]-num
+                
+                
+            return arr
+                
+        
+        def func(arr):
+            heap=[]
+            for i in arr:
+                if i>0:
+                    heap.append(i)
+                    
+            if len(heap)>0:
+                return min(heap)
+            else:
+                return 0
+            
+        op=0  
+        while sum(nums)!=0:
+            op+=1
+            x=func(nums)
+            nums[:]=func1(nums,x)
+            
+        return op
+        
+        
