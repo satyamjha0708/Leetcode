4638da8d9919cce0cb21a0731a473ed7b0694f89
Satyam Jha <61003236+satyamjha0708@users.noreply.github.com>
2022-08-08 14:22:23 +0530

    Time: 1338 ms (67.88%), Space: 32.1 MB (51.95%) - LeetHub

--- 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.py
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.py
@@ -1,22 +1,17 @@
 class Solution:
     def countExcellentPairs(self, nums: List[int], k: int) -> int:
-        def func(i):
-                return bin(i)[2:].count('1')
-        nums=set(nums)
-        n=len(nums)
-        m=[]
-        for i in nums:
-                m.append(func(i))
+        
+        nums = set(nums)
+        count1 = sorted([bin(num)[2:].count("1") for num in nums])
+        
+        res = 0
+        n = len(count1)
+        print(count1)
+        for val in count1:
+                remain = k - val
+                idx = bisect.bisect_left(count1, remain)
+                res += n - idx
                 
-        m.sort()
-        res=0
-        for i in m:
-                if i>=k:
-                        res+=n
-                else:
-                        idx=bisect.bisect_left(m,k-i)
-                        res+=n-idx
-                        
-                        
-                        
         return res
+
+        
