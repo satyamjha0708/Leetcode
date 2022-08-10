cb3e6f1b12fd56ab9128da283891dd97ef453de1
Satyam Jha <61003236+satyamjha0708@users.noreply.github.com>
2022-08-11 02:15:31 +0530

    Time: 153 ms (85.00%), Space: 15 MB (59.20%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        farthest=0
+        current=0
+        jump=0
+        
+        for i in range(len(nums)-1):
+                farthest=max(farthest,i+nums[i])
+                
+                if i==current:
+                        current=farthest
+                        jump+=1
+                        
+                        
+        return jump
+                
