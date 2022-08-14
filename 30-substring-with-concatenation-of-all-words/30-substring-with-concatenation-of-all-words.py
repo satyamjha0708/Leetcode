83613182cd018e70927122bf34aacace9d344dea
Satyam Jha <61003236+satyamjha0708@users.noreply.github.com>
2022-08-14 17:16:10 +0530

    Time: 1883 ms (22.31%), Space: 14.3 MB (38.75%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
@@ -1,36 +1,40 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        def solve(s,words):
-            found = {}
-            for word in words:
-                found[word] = found.get(word,0)+1
-            k = len(words[0])
-            ws = 0
-            last = 0
-            result = []
-            length = 0
-            while last<len(s)-k+1:
-                if s[last:last+k] in found and found[s[last:last+k]]!=0:
-                    # print(s[last:last+k],last,ws,found)
-                    found[s[last:k+last]] -= 1 
-                    if found[s[last:k+last]] == 0:
-                        length += 1 
-                    last = last + k 
-                    if length == len(found):
-                        result.append(ws)
-                        ws += 1 
-                        last = ws
-                        length = 0
-                        found = {}
-                        for word in words:
-                            found[word] = found.get(word,0)+ 1
+        d={}
+        for i in words:
+                d[i]=d.get(i,0)+1
+                
+        k=len(words[0])
+        w_s=0
+        lenght=0
+        result=[]
+        last=0
+        while last<len(s)-k+1:
+                if s[last:last+k] in d and d[s[last:last+k]]!=0:
+                        d[s[last:last+k]]-=1
+                        
+                        if d[s[last:last+k]]==0:
+                                lenght+=1
+                                
+                        last+=k    
+                        if lenght==len(d):
+                                result.append(w_s)
+                                w_s+=1
+                                
+                                last=w_s
+                                lenght=0
+                                d={}
+                                for i in words:
+                                        d[i]=d.get(i,0)+1
+                                        
+                                        
                 else:
-                    # print(found,last,ws)
-                    ws += 1 
-                    last = ws
-                    length = 0
-                    found = {}
-                    for word in words:
-                        found[word] = found.get(word,0)+1
-            return result
-        return solve(s,words)
+                        w_s+=1
+                        last=w_s
+                        lenght=0
+                        d={}
+                        for i in words:
+                                d[i]=d.get(i,0)+1
+                                
+        return result
+                                
